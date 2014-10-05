a0dc04150557bc043ae1fc5447ded56fe9102ba1
Tung-keng (Cyril) Lee <cyril76928@gmail.com>
2014-10-05 16:04:00 -0700

    for new project structure

---
+++ src/main/java/edu/cmu/ini/impli_auth/server/Device.java
@@ -0,0 +1,38 @@
+package edu.cmu.ini.impli_auth.server;
+
+public class Device {
+
+	String name;
+	int strength;
+	String phy_attr;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	
+	public String getPhyAttr(){
+		return phy_attr;
+	}
+	
+	public void setPhyAttr(String id){
+		this.phy_attr = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Device [name=" + name + ", strength=" + strength + ", phy_attr="+ phy_attr + "]";
+	}
+
+}
