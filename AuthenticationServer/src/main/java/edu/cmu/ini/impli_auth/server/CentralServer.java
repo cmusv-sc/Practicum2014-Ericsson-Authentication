b55be165344433f60a98fa14b87880eab7475c81
Tung-keng (Cyril) Lee <cyril76928@gmail.com>
2014-11-10 00:35:21 -0800

    add facial recognition training and testing into server

--- AuthenticationServer/src/main/java/edu/cmu/ini/impli_auth/server/CentralServer.java
+++ AuthenticationServer/src/main/java/edu/cmu/ini/impli_auth/server/CentralServer.java
@@ -1,5 +1,5 @@
 package edu.cmu.ini.impli_auth.server;
- 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -16,10 +16,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.bind.DatatypeConverter;
- 
+
+import edu.cmu.ini.impli_auth.face.AuthFaceRecognizer;
+
 @Path("/json")
 public class CentralServer {
-	
+
 	@GET
 	@Path("/db")
 	public void getdata() {
@@ -31,64 +33,40 @@ public class CentralServer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@POST
-	@Path("/pic")
+	@Path("/trainImage")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response postImage(@FormParam("id") String id_str, @FormParam("image") String image) {
-		System.out.println(id_str);
-		//System.out.println(image);
-		int id = Integer.parseInt(id_str);
+	public Response postTrainImage(@FormParam("image") String image) {
 		byte[] imageData = DatatypeConverter.parseBase64Binary(image);
 
 		FileOutputStream outStream = null;
-/*
-		// Write to local (for test)
-		try {		
-		
-			String fileName = String.format("%d.jpg", System.currentTimeMillis());
-			File outFile = new File(fileName);
-			outStream = new FileOutputStream(outFile);
-			outStream.write(imageData);
-			outStream.flush();
-			outStream.close();
-			
-			sqlConnection dao = new sqlConnection();
-			try {
-				ResultSet resultSet = dao.readDataBase(id);
-				String picture = null;
-				if(resultSet.next()) {
-					AuthFaceRecognizer faceRecognizer = new AuthFaceRecognizer(outFile);
-					faceRecognizer.run();
-					picture = resultSet.getString("PICTURE");
-					picture += ";" + fileName;
-					dao.updateDataBase(id, picture);
-				}
-				else {
-					picture = fileName;
-					dao.writeDataBase(id, picture);
-				}
-				return Response.status(200).entity("This user is " + id).build();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-		}
-		*/
-		AuthFaceRecognizer faceRecognizer = new AuthFaceRecognizer(new File(""));
-		faceRecognizer.run();
-		return Response.status(200).entity("the request is failed").build();
+		/*
+		 * // Write to local (for test) try {
+		 * 
+		 * String fileName = String.format("%d.jpg",
+		 * System.currentTimeMillis()); File outFile = new File(fileName);
+		 * outStream = new FileOutputStream(outFile);
+		 * outStream.write(imageData); outStream.flush(); outStream.close();
+		 * 
+		 * sqlConnection dao = new sqlConnection(); try { ResultSet resultSet =
+		 * dao.readDataBase(id); String picture = null; if(resultSet.next()) {
+		 * AuthFaceRecognizer faceRecognizer = new AuthFaceRecognizer(outFile);
+		 * faceRecognizer.run(); picture = resultSet.getString("PICTURE");
+		 * picture += ";" + fileName; dao.updateDataBase(id, picture); } else {
+		 * picture = fileName; dao.writeDataBase(id, picture); } return
+		 * Response.status(200).entity("This user is " + id).build(); } catch
+		 * (Exception e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 * 
+		 * } catch (FileNotFoundException e) { e.printStackTrace(); } catch
+		 * (IOException e) { e.printStackTrace(); } finally { }
+		 */
+		return Response.status(200).entity("post image succeed").build();
 	}
-	
-	
+
 	@GET
 	@Path("/getUser")
 	@Produces("application/json")
@@ -100,11 +78,11 @@ public class CentralServer {
 		user.setEmail("alok@email.com");
 		user.setUserName("alok");
 		user.setPassword("password");
-		
-		return user; 
+
+		return user;
 
 	}
-	
+
 	@GET
 	@Path("/getDevice")
 	@Produces("application/json")
@@ -114,11 +92,11 @@ public class CentralServer {
 		device.setName("Phone");
 		device.setStrength(10);
 		device.setPhyAttr("IMEI");
-		
-		return device; 
+
+		return device;
 
 	}
-	
+
 	@GET
 	@Path("/getResource")
 	@Produces("application/json")
@@ -130,11 +108,11 @@ public class CentralServer {
 		resource.setName("TV");
 		resource.setNSSID("CMU");
 		resource.setType("display");
-		
-		return resource; 
+
+		return resource;
 
 	}
-	
+
 	@GET
 	@Path("/getActiveUser")
 	@Produces("application/json")
@@ -146,11 +124,11 @@ public class CentralServer {
 		user.setNSSID("CMU");
 		user.setID(1);
 		user.setUser_ID(1);
-		
-		return user; 
+
+		return user;
 
 	}
-	
+
 	@POST
 	@Path("/postUser")
 	@Consumes("application/json")
@@ -158,9 +136,9 @@ public class CentralServer {
 
 		String result = "User created : " + user;
 		return Response.status(201).entity(result).build();
-		
+
 	}
-	
+
 	@POST
 	@Path("/postDevice")
 	@Consumes("application/json")
@@ -168,9 +146,9 @@ public class CentralServer {
 
 		String result = "Device created : " + device;
 		return Response.status(201).entity(result).build();
-		
+
 	}
-	
+
 	@POST
 	@Path("/postResource")
 	@Consumes("application/json")
@@ -178,9 +156,9 @@ public class CentralServer {
 
 		String result = "Resource created : " + resource;
 		return Response.status(201).entity(result).build();
-		
+
 	}
-	
+
 	@POST
 	@Path("/postLocation")
 	@Consumes("application/json")
@@ -195,8 +173,20 @@ public class CentralServer {
 			e.printStackTrace();
 		}
 		return Response.status(201).entity(result).build();
-		
+
 	}
-	
-	
+
+	@POST
+	@Path("/testImage")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response processTestImage(@FormParam("width") int width,
+			@FormParam("height") int height, @FormParam("image") String image) {
+		byte[] imageData = DatatypeConverter.parseBase64Binary(image);
+
+		AuthFaceRecognizer faceRecognizer = new AuthFaceRecognizer(width,
+				height);
+		faceRecognizer.train();
+		faceRecognizer.test(imageData);
+		return Response.status(200).entity("the request is ok").build();
+	}
 }
