8f9d393c3f793f67cc2f2aaa4494c035dd0dc058
Tung-keng (Cyril) Lee <cyril76928@gmail.com>
2014-11-17 14:25:01 -0800

    add FaceTestResult.java

---
+++ AuthenticationServer/src/main/java/edu/cmu/ini/impli_auth/server/FaceTestResult.java
@@ -0,0 +1,10 @@
+package edu.cmu.ini.impli_auth.server;
+
+public class FaceTestResult {
+	int label;
+	double p;
+	public FaceTestResult(int label, double p) {
+		this.label = label;
+		this.p = p;
+	}
+}
