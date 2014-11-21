dbfe722acb0b43b82be1f01730789d456014f8e1
Yifeng Huang <ivanhyf@gmail.com>
2014-11-20 17:36:49 -0800

    Fix wrong logic in share resource login activity

--- SharedResource/app/src/main/java/edu/cmu/ini/impli_auth/face/LoginActivity.java
+++ SharedResource/app/src/main/java/edu/cmu/ini/impli_auth/face/LoginActivity.java
@@ -139,7 +139,7 @@ public class LoginActivity extends Activity{
 
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password)) {
+        if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
             cancel = true;
