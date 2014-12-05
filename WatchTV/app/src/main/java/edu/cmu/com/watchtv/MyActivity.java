a996c919e9443f20afb81e2bdd0165dea1b2cf32
Tung-keng (Cyril) Lee <cyril76928@gmail.com>
2014-12-04 16:11:24 -0800

    format WatchTV

--- WatchTV/app/src/main/java/edu/cmu/com/watchtv/MyActivity.java
+++ WatchTV/app/src/main/java/edu/cmu/com/watchtv/MyActivity.java
@@ -11,49 +11,49 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MyActivity extends Activity {
-    boolean pressed = false;
-    Button myButton;
-    EditText myText;
-    EditText myText2;
+	boolean pressed = false;
+	Button myButton;
+	EditText myText;
+	EditText myText2;
 	private String TAG = "MyActivity";
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-        EditText myText = (EditText) findViewById(R.id.editText);
-        final EditText myText2 = (EditText) findViewById(R.id.editText2);
-        final Button myButton = (Button) findViewById(R.id.my_button);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent();
-                i.setComponent(new ComponentName("edu.cmu.ini.impli_auth.auth_client",
-		                "edu.cmu.ini.impli_auth.auth_client.face.FaceActivity"));
-                startActivityForResult(i, 1);
-            }
-        });
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my);
+		EditText myText = (EditText) findViewById(R.id.editText);
+		final EditText myText2 = (EditText) findViewById(R.id.editText2);
+		final Button myButton = (Button) findViewById(R.id.my_button);
+		myButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent();
+				i.setComponent(new ComponentName("edu.cmu.ini.impli_auth.auth_client",
+						"edu.cmu.ini.impli_auth.auth_client.face.FaceActivity"));
+				startActivityForResult(i, 1);
+			}
+		});
+	}
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        String location = data.getStringExtra("Access");
-        if (requestCode == 1 ) {
-            if(resultCode == RESULT_OK){
-                Intent intent = new Intent(MyActivity.this, PlayVideo.class);
-                Log.d(TAG, String.valueOf(data.getDoubleExtra("Prob", -1.0)));
-                Log.d(TAG, data.getStringExtra("UserName"));
-                Log.d(TAG, location);
-                intent.putExtra("probability", data.getDoubleExtra("Prob", -1.0));
-                intent.putExtra("username", data.getStringExtra("UserName"));
-                intent.putExtra("access",location);
-                startActivity(intent);
-            }
+		String location = data.getStringExtra("Access");
+		if (requestCode == 1) {
+			if (resultCode == RESULT_OK) {
+				Intent intent = new Intent(MyActivity.this, PlayVideo.class);
+				Log.d(TAG, String.valueOf(data.getDoubleExtra("Prob", -1.0)));
+				Log.d(TAG, data.getStringExtra("UserName"));
+				Log.d(TAG, location);
+				intent.putExtra("probability", data.getDoubleExtra("Prob", -1.0));
+				intent.putExtra("username", data.getStringExtra("UserName"));
+				intent.putExtra("access", location);
+				startActivity(intent);
+			}
 
-        }
-                if (resultCode == RESULT_CANCELED) {
-                    Toast.makeText(this, "To use this service please register with Pyxis", Toast.LENGTH_LONG).show();
-                }
-        }
+		}
+		if (resultCode == RESULT_CANCELED) {
+			Toast.makeText(this, "To use this service please register with Pyxis", Toast.LENGTH_LONG).show();
+		}
+	}
 
 
-    }
+}
