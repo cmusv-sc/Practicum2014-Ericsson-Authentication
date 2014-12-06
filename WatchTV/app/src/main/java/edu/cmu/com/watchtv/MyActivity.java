b59327c0ab9ae34d4aa6f3085d92f4c0e2c80bff
Tung-keng (Cyril) Lee <cyril76928@gmail.com>
2014-12-06 01:44:27 -0800

    reformat WatchTV

--- WatchTV/app/src/main/java/edu/cmu/com/watchtv/MyActivity.java
+++ WatchTV/app/src/main/java/edu/cmu/com/watchtv/MyActivity.java
@@ -14,44 +14,44 @@ import android.widget.Toast;
  * to PlayVideo activity.
  */
 public class MyActivity extends Activity {
-    
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
 
-        final Button myButton = (Button) findViewById(R.id.my_button);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent();
-                i.setComponent(new ComponentName("edu.cmu.ini.impli_auth.auth_client",
-		                "edu.cmu.ini.impli_auth.auth_client.face.FaceActivity"));
-                startActivityForResult(i, 1);
-            }
-        });
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my);
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		final Button myButton = (Button) findViewById(R.id.my_button);
+		myButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent();
+				i.setComponent(new ComponentName("edu.cmu.ini.impli_auth.auth_client",
+						"edu.cmu.ini.impli_auth.auth_client.face.FaceActivity"));
+				startActivityForResult(i, 1);
+			}
+		});
+	}
 
-        String location = data.getStringExtra("Access");
-        if (requestCode == 1 ) {
-            if(resultCode == RESULT_OK){
-                Intent intent = new Intent(MyActivity.this, PlayVideo.class);
-                String TAG = "MyActivity";
-                Log.d(TAG, String.valueOf(data.getDoubleExtra("Prob", -1.0)));
-                Log.d(TAG, data.getStringExtra("UserName"));
-                Log.d(TAG, location);
-                intent.putExtra("probability", data.getDoubleExtra("Prob", -1.0));
-                intent.putExtra("username", data.getStringExtra("UserName"));
-                intent.putExtra("access",location);
-                startActivity(intent);
-            }
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        }
-                if (resultCode == RESULT_CANCELED) {
-                    Toast.makeText(this, "To use this service please register with Pyxis", Toast.LENGTH_LONG).show();
-                }
-        }
+		String location = data.getStringExtra("Access");
+		if (requestCode == 1) {
+			if (resultCode == RESULT_OK) {
+				Intent intent = new Intent(MyActivity.this, PlayVideo.class);
+				String TAG = "MyActivity";
+				Log.d(TAG, String.valueOf(data.getDoubleExtra("Prob", -1.0)));
+				Log.d(TAG, data.getStringExtra("UserName"));
+				Log.d(TAG, location);
+				intent.putExtra("probability", data.getDoubleExtra("Prob", -1.0));
+				intent.putExtra("username", data.getStringExtra("UserName"));
+				intent.putExtra("access", location);
+				startActivity(intent);
+			}
+
+		}
+		if (resultCode == RESULT_CANCELED) {
+			Toast.makeText(this, "To use this service please register with Pyxis", Toast.LENGTH_LONG).show();
+		}
+	}
 
 
-    }
+}
--- WatchTV/app/src/main/java/edu/cmu/com/watchtv/ViewVideo.java
+++ WatchTV/app/src/main/java/edu/cmu/com/watchtv/ViewVideo.java
@@ -16,45 +16,45 @@ import android.widget.VideoView;
  * Full screen view for video.
  */
 public class ViewVideo extends Activity {
-    private static final int INSERT_ID = Menu.FIRST;
+	private static final int INSERT_ID = Menu.FIRST;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        System.gc();
-        Intent i = getIntent();
-        Bundle extras = i.getExtras();
-        String filename = extras.getString("videofilename");
-        VideoView vv = new VideoView(getApplicationContext());
-        setContentView(vv);
-        vv.setVideoPath(filename);
-        vv.setMediaController(new MediaController(this));
-        vv.requestFocus();
-        vv.start();
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		System.gc();
+		Intent i = getIntent();
+		Bundle extras = i.getExtras();
+		String filename = extras.getString("videofilename");
+		VideoView vv = new VideoView(getApplicationContext());
+		setContentView(vv);
+		vv.setVideoPath(filename);
+		vv.setMediaController(new MediaController(this));
+		vv.requestFocus();
+		vv.start();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        menu.add(0, INSERT_ID, 0,"FullScreen");
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, INSERT_ID, 0, "FullScreen");
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public boolean onMenuItemSelected(int featureId, @NonNull MenuItem item) {
-        switch(item.getItemId()) {
-            case INSERT_ID:
-                createNote();
-        }
-        return true;
-    }
+	@Override
+	public boolean onMenuItemSelected(int featureId, @NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case INSERT_ID:
+				createNote();
+		}
+		return true;
+	}
 
-    @SuppressLint("AppCompatMethod")
-    private void createNote() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
+	@SuppressLint("AppCompatMethod")
+	private void createNote() {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
 
 }
